811f51dc71b5e3dd784a878a4421cd8b67b37c96
Lucas Nogueira <lucas1020111@gmail.com>
2021-12-18 19:42:17 -0300

    Adding component Stock

--- src/components/Stock/index.vue
+++ src/components/Stock/index.vue
@@ -1,9 +1,59 @@
-<template lang="">
-  <h1>Action</h1>
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>{{ stock.name }} <small>(Price: {{ stock.price }})</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          name="quantity"
+          label="Quantity"
+          type="number"
+          v-model.number="quantity"
+        />
+        <v-btn 
+          class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock"
+        >
+          Buy
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 <script>
 export default {
-  
+  props: {
+    stock: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      quantity: 0,
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      console.log(order);
+      this.clearQuantity();
+    },
+    sellStock() {
+
+    },
+    clearQuantity() {
+      this.quantity = 0;
+    }
+  },
 }
 </script>
 <style lang="">
--- src/pages/StockList/index.vue
+++ src/pages/StockList/index.vue
@@ -1,9 +1,22 @@
 <template lang="">
-  <h1> Actions </h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
+  </v-layout>
 </template>
 <script>
+import Stock from '../../components/Stock';
 export default {
-  
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 280},
+        {id: 4, name: 'Twitter', price: 83},
+      ],
+    }
+  }
 }
 </script>
 <style lang="">
